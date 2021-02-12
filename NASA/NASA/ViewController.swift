9ace9b191d6843b61a53cac8debcdbc75ade4ff4
Matheus Fusco <site_02@prestadorcbmp.com.br>
2021-02-12 18:05:26 -0300

    Configuração inicial do projeto. Git ignore adicionado. Gestão de dependências adicionado - Cocoapods.

---
+++ NASA/NASA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NASA
+//
+//  Created by Matheus Pacheco Fusco on 12/02/21.
+//  Copyright © 2021 Matheus Pacheco Fusco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
