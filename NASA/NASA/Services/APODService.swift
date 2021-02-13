2d8fae8a26a094f2dc6967f33a594890910f1b25
Matheus Fusco <site_02@prestadorcbmp.com.br>
2021-02-13 14:00:06 -0300

    APODService atualizado - agora o retorno está ajustado (havia colocado como array para teste da chamada com o parâmetro 'count').

--- NASA/NASA/Services/APODService.swift
+++ NASA/NASA/Services/APODService.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class APODService: BaseService<[APOD]> {
+class APODService: BaseService<APOD> {
     func getPictureOfTheDay(success: SuccessHandler, failure: FailureHandler) {
         let serviceUrl = "\(Constants.Api.baseUrl)planetary/apod"
         self.get(url: serviceUrl, success: success, failure: failure)
