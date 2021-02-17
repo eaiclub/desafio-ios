3ae91a56bec551b2945e3291398163ffa298691b
grleitao <t0705gst@prestadorcbmp.com.br>
2021-02-17 09:33:05 -0300

    UIView extension preparing view height and conerRadius

--- NASA/NASA/Commons/Extensions/UIViewExtension.swift
+++ NASA/NASA/Commons/Extensions/UIViewExtension.swift
@@ -25,3 +25,64 @@ extension UIView {
         view.layoutIfNeeded()
     }
 }
+
+extension UIView {
+
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            let color = UIColor.init(cgColor: layer.borderColor!)
+            return color
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
+
+extension UIView {
+    
+   func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
+    }
+}
+
+extension UIView {
+    func addHeightConstaintOrUpdate(constant : CGFloat) {
+        if let constraint = (self.constraints.filter({$0.identifier == "height"}).first) {
+            constraint.constant = constant
+            self.frame.size.height = constant
+        }else {
+            self.frame.size.height = constant
+            let heightConstraint = heightAnchor.constraint(equalToConstant: frame.size.height)
+            heightConstraint.identifier = "height"
+            heightConstraint.priority   = UILayoutPriority(250)
+            heightConstraint.isActive   = true
+        }
+        self.setNeedsLayout()
+        self.invalidateIntrinsicContentSize()
+    }
+}
