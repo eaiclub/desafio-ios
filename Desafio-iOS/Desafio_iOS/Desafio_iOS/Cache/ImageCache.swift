149168032e35110261f6027720a0fb2717828954
Pedro Feitosa <you@example.com>
2021-03-30 20:02:46 -0300

    ImageCache implementado

---
+++ Desafio-iOS/Desafio_iOS/Desafio_iOS/Cache/ImageCache.swift
@@ -0,0 +1,51 @@
+//
+//  ImageCache.swift
+//  Desafio_iOS
+//
+//  Created by Pedro Feitosa on 30/03/21.
+//
+
+import Foundation
+import UIKit
+
+class ImageCache{
+    
+    
+    var imgCache = NSCache<NSString,UIImage>()
+    private var dataTask : URLSessionDataTask?
+    
+    func downloadImage(url: URL, completion: @escaping (UIImage?) -> Void){
+        
+        if let cachedImage = imgCache.object(forKey: url.absoluteString as NSString) {
+            completion(cachedImage)
+            
+        } else {
+            
+            let request = URLRequest(url: url, cachePolicy: URLRequest.CachePolicy.returnCacheDataElseLoad, timeoutInterval: 0)
+            dataTask = URLSession.shared.dataTask(with: request) {  [weak self] data, response, error  in
+                
+                guard error == nil, data != nil else {
+                    return
+                }
+                
+                guard let image = UIImage(data: data!) else { return }
+                
+                self!.imgCache.setObject(image, forKey: url.absoluteString as NSString)
+                print("SALVOU A IMG: \(url.absoluteString)")
+                
+                DispatchQueue.main.async {
+                    completion(image)
+                }
+                
+            }
+            dataTask?.resume()
+        }
+    }
+
+    
+    
+    
+    
+    
+}
+
