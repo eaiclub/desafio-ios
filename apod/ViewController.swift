b3626d88536384ee9123dc376d56cdd7a9cfe5ee
Rafael Rollo <rafaelrollo92@gmail.com>
2021-09-22 17:07:08 -0300

    refactor: configure the project to favor view coding instead of storyboard usage

--- apod/ViewController.swift
+++ apod/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override func loadView() {
+        super.loadView()
+        
+        view.backgroundColor = .systemBackground
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
