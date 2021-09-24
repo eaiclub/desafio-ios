694659668642a4059b60392ffea2e94bb2ac4be9
Rafael Rollo <rafaelrollo92@gmail.com>
2021-09-24 09:14:20 -0300

    feat(calendar): create some helper to calculate days between dates

---
+++ apod/Common/Extensions/DaysBetween+Calendar.swift
@@ -0,0 +1,18 @@
+//
+//  DaysBetween+Calendar.swift
+//  apod
+//
+//  Created by rafael.rollo on 23/09/21.
+//
+
+import Foundation
+
+extension Calendar {
+    func numberOfDaysBetween(_ from: Date, and to: Date) -> Int {
+        let fromDate = startOfDay(for: from)
+        let toDate = startOfDay(for: to)
+        let numberOfDays = dateComponents([.day], from: fromDate, to: toDate)
+        
+        return numberOfDays.day! + 1
+    }
+}
